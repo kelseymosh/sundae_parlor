561a9bb40f6b5f9cacc24bfb914a0ac3799f8cce
Kelsey Oshiro <kelseymosh@gmail.com>
2020-09-30 15:28:04 -0600

    all first changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,24 +2,24 @@ IceCream.destroy_all
 Topping.destroy_all
 Sundae.destroy_all
 
-chocolate = IceCream.create(flavor: "Chocolate")
-strawberry = IceCream.create(flavor: "Strawberry")
-vanilla = IceCream.create(flavor: "Vanilla")
-pistachio = IceCream.create(flavor: "Pistachio")
-cookie_dough = IceCream.create(flavor: "Cookie Dough")
-dulce_de_leche = IceCream.create(flavor: "Dulce de Leche")
-rainbow = IceCream.create(flavor: "Rainbow")
-cake_batter = IceCream.create(flavor: "Cake Batter")
+chocolate = IceCream.create(flavor: "chocolate")
+strawberry = IceCream.create(flavor: "strawberry")
+vanilla = IceCream.create(flavor: "vanilla")
+pistachio = IceCream.create(flavor: "pistachio")
+cookie_dough = IceCream.create(flavor: "cookie dough")
+dulce_de_leche = IceCream.create(flavor: "dulce de leche")
+rainbow = IceCream.create(flavor: "rainbow")
+cake_batter = IceCream.create(flavor: "cake batter")
 
-marshmallows = Topping.create(topping_type: "Marshmallows")
-pretzels = Topping.create(topping_type: "Pretzels")
-chocolate_chips = Topping.create(topping_type: "Chocolate Chips")
-oreos = Topping.create(topping_type: "Oreos")
-sprinkles = Topping.create(topping_type: "Sprinkles")
-hot_fudge = Topping.create(topping_type: "Hot Fudge")
-reeses_cups = Topping.create(topping_type: "Reeses Cups")
-brownies = Topping.create(topping_type: "Brownies")
-cheese_cake_bites = Topping.create(topping_type: "Cheese Cake Bites")
+marshmallows = Topping.create(topping_type: "marshmallows")
+pretzels = Topping.create(topping_type: "pretzels")
+chocolate_chips = Topping.create(topping_type: "chocolate chips")
+oreos = Topping.create(topping_type: "oreos")
+sprinkles = Topping.create(topping_type: "sprinkles")
+hot_fudge = Topping.create(topping_type: "hot fudge")
+reeses_cups = Topping.create(topping_type: "reeses cups")
+brownies = Topping.create(topping_type: "brownies")
+cheese_cake_bites = Topping.create(topping_type: "cheese cake bites")
 
 
 binding.pry
--- runner.rb
+++ runner.rb
@@ -5,5 +5,7 @@ cli = Cli.new
 cli.start_app
 # cli.show_flavors
 cli.choose_a_sample
-
+cli.order_ice_cream
+cli.pick_toppings
+cli.create_sundae
 
